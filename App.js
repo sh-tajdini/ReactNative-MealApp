fef359658134290a543165ee12eaa6deb5250b35
Shervin Tajdini <shervin.taj@gmail.com>
2023-10-23 15:25:19 -0400

    clean code

--- App.js
+++ App.js
@@ -38,12 +38,6 @@ export default function App() {
         <Stack.Screen 
         name="MealsOverview" 
         component={MealsOverviewScreen} 
-        // options={({route,navigation})=>{
-        //   const catId = route.params.categoryId;
-        //   return {
-        //     title: catId,
-        //   };
-        // }}
         />
         <Stack.Screen name="MealDetail" 
         component={MealDetailScreen}
